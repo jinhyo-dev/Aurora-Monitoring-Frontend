fee561afaa36bd5ca8f9aaf1c28bbef5ef894f62
Jinhyo Kim <admin@jinhyo.dev>
2023-06-08 23:42:12 +0900

    feat: Change server port and add items in SignUp

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -19,9 +19,18 @@ const SignUp = () => {
       </Title>
 
       <PlanContainer>
-        <PlanBox></PlanBox>
-        <PlanBox style={{ marginLeft: '5%' }}></PlanBox>
-        <PlanBox style={{ marginLeft: '5%' }}></PlanBox>
+        <PlanBox>
+          <PlanName>Free</PlanName>
+          <PlanButton>Continue with Free</PlanButton>
+        </PlanBox>
+        <PlanBox style={{ marginLeft: '5%' }}>
+          <PlanName>Team</PlanName>
+          <PlanButton>Continue with Team</PlanButton>
+        </PlanBox>
+        <PlanBox style={{ marginLeft: '5%' }}>
+          <PlanName>Enterprise</PlanName>
+          <PlanButton>Continue with Enterprise</PlanButton>
+        </PlanBox>
       </PlanContainer>
 
 
@@ -33,22 +42,47 @@ const Title = styled.div`
   margin-top: 2rem;
   text-align: center;
   font-size: 2rem;
-  font-weight: 600;
+  font-weight: 500;
 `
 
 const PlanContainer = styled.div`
   width: 90%;
   margin: calc(6% - 3rem) auto 0;
   height: 35rem;
+  text-align: center;
+`
+
+const PlanName= styled.div`
+  font-size: 1.8rem;
+  text-align: center;
+  margin-top: 2rem;
+  font-weight: 600;
+`
+
+const PlanButton = styled.button`
+  margin: 2rem auto 0;
+  width: 90%;
+  height: 3rem;
+  font-size: 1.1rem;
+  border: 2px solid #fff;
+  border-radius: 5px;
+  background: none;
+  color: #fff;
+  cursor: pointer;
+  transition: all .2s;
+  
+  &:hover {
+    background: #fff;
+    color: #000;
+  }
 `
 
 const PlanBox = styled.div`
   width: 30%;
   height: 100%;
   float: left;
-  background-color: #000;
+  background-color: rgba(0, 0, 0, .6);
   border-radius: 7px;
-  opacity: .6;
 `
 
 export default SignUp
